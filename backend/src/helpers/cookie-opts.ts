88cd21488cd75c380306a1825aadcc5196dfde7f
root <root@cv4625001.novalocal>
2025-04-05 22:54:11 +0300

    basic knoledge

--- backend/src/helpers/cookie-opts.ts
+++ backend/src/helpers/cookie-opts.ts
@@ -3,7 +3,7 @@ import { CookieListItem } from "@whatwg-node/cookie-store";
 export const cookieOpts: CookieListItem = {
   httpOnly: true,
   sameSite: 'lax',
-  secure: import.meta.env.DEV ? false : true,
+  secure: import.meta.env.DEV ? false : false,
   path: '/',
   domain: null,
   expires: null,
