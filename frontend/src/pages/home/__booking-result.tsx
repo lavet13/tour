c70b5f84d485fc204f87a176d7495c4b02d078fa
lavet13 <lavet13@mail.ru>
2025-05-03 21:57:35 +0300

    fix styling: center the text

--- frontend/src/pages/home/__booking-result.tsx
+++ frontend/src/pages/home/__booking-result.tsx
@@ -3,12 +3,12 @@ import { FC } from 'react';
 
 const BookingResult: FC = () => {
   return (
-    <div className='space-y-4 px-4 p-5 md:py-6 md:p-12'>
+    <div className='flex-1 space-y-4 px-4 p-5 md:py-6 md:p-12'>
       <div className='p-6 flex flex-col items-center'>
         <div className='rounded-full bg-foreground p-3 mb-4'>
           <CheckCircle className='h-8 w-8 text-background' />
         </div>
-        <h2 className='text-2xl font-bold text-background-700 mb-2'>
+        <h2 className='text-center text-2xl font-bold text-background-700 mb-2'>
           Заявка подтверждена!
         </h2>
         <p className='text-background-600 text-center'>
