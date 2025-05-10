c4f50af9892e55174ddf7eae3d6cfcfef8e48ae5
lavet13 <lavet13@mail.ru>
2025-05-10 19:28:52 +0300

    test if telegram-mini-app is serving under domain

--- telegram-mini-app/vite.config.ts
+++ telegram-mini-app/vite.config.ts
@@ -5,7 +5,7 @@ import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/reactjs-template/',
+  base: '/telegram-mini-app/',
   css: {
     preprocessorOptions: {
       scss: {
