f9bdb5ad140064eb8dca8cdaf02296776658dc0a
lavet13 <lavet13@mail.ru>
2025-08-09 00:12:57 +0300

    rolling back

--- backend/src/services/grammy/api/api.ts
+++ backend/src/services/grammy/api/api.ts
@@ -1,8 +1,5 @@
 import { BotError, Composer } from 'grammy';
-import {
-  handleBookingSendMessage,
-  handleBookingStatus,
-} from '@/services/grammy/api/bookings/middlewares';
+import { handleBookingStatus } from '@/services/grammy/api/bookings/middlewares';
 import { CustomContext } from '@/services/grammy/types';
 
 const api = new Composer();
@@ -11,7 +8,6 @@ api
   .errorBoundary((error: BotError<CustomContext>) => {
     console.error('Error in booking API:', error);
   })
-  .callbackQuery(/booking:send-message_(.*)/, handleBookingSendMessage)
   .callbackQuery(/booking:status_(.*)/, handleBookingStatus);
 
 export { api };
