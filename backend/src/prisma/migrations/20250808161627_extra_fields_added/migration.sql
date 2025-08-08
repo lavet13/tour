a7abb2b8c485536b2548175574c0e3b790886408
lavet13 <lavet13@mail.ru>
2025-08-08 19:16:50 +0300

    extra fields?

--- backend/src/prisma/migrations/20250808161627_extra_fields_added/migration.sql
+++ backend/src/prisma/migrations/20250808161627_extra_fields_added/migration.sql
@@ -1,13 +1,11 @@
 /*
   Warnings:
 
-  - You are about to drop the column `hash` on the `telegram_users` table. All the data in the column will be lost.
   - Added the required column `allowsWriteToPm` to the `telegram_users` table without a default value. This is not possible if the table is not empty.
 
 */
 -- AlterTable
-ALTER TABLE "telegram_users"
-ADD COLUMN     "allowsWriteToPm" BOOLEAN NOT NULL,
+ALTER TABLE "telegram_users" ADD COLUMN     "allowsWriteToPm" BOOLEAN NOT NULL,
 ADD COLUMN     "chatInstance" BIGINT,
 ADD COLUMN     "chatType" TEXT,
 ADD COLUMN     "languageCode" TEXT;
